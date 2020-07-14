ff9dc503cf3c51ca152a3a333c4b90bdc70b0f69
Enver Haase <enver@vaadin.com>
2020-07-14 23:32:27 +0200

    Further developed the I64 Reader.

--- imageio/imageio-i64/src/main/java/com/twelvemonkeys/imageio/plugins/i64/I64ImageReader.java
+++ imageio/imageio-i64/src/main/java/com/twelvemonkeys/imageio/plugins/i64/I64ImageReader.java
@@ -31,32 +31,19 @@
 package com.twelvemonkeys.imageio.plugins.i64;
 
 import com.twelvemonkeys.imageio.ImageReaderBase;
-import com.twelvemonkeys.imageio.util.IIOUtil;
 import com.twelvemonkeys.imageio.util.ImageTypeSpecifiers;
-import com.twelvemonkeys.io.enc.DecoderStream;
-import com.twelvemonkeys.xml.XMLSerializer;
 
-import javax.imageio.IIOException;
-import javax.imageio.ImageIO;
 import javax.imageio.ImageReadParam;
 import javax.imageio.ImageTypeSpecifier;
 import javax.imageio.metadata.IIOMetadata;
-import javax.imageio.metadata.IIOMetadataFormatImpl;
 import javax.imageio.spi.ImageReaderSpi;
 import javax.imageio.stream.ImageInputStream;
-import java.awt.*;
-import java.awt.color.ColorSpace;
-import java.awt.image.*;
-import java.io.DataInput;
-import java.io.DataInputStream;
-import java.io.File;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 import java.io.IOException;
-import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
-import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 
 /**
  * ImageReader for ZSoft PC Paintbrush (PCX) format.
@@ -67,11 +54,12 @@ public final class I64ImageReader extends ImageReaderBase {
 
     final static boolean DEBUG = "true".equalsIgnoreCase(System.getProperty("com.twelvemonkeys.imageio.plugins.i64.debug"));
 
-    /** 8 bit ImageTypeSpecifer used for reading bitplane images. */
-    private static final ImageTypeSpecifier GRAYSCALE = ImageTypeSpecifiers.createGrayscale(8, DataBuffer.TYPE_BYTE);
 
-    private boolean readPalette;
-    private IndexColorModel vgaPalette;
+    private static final int BUFFERED_IMAGE_TYPE_RGB555 = BufferedImage.TYPE_USHORT_555_RGB;
+    /**
+     * 8 bit ImageTypeSpecifier used for reading bitplane images.
+     */
+    private static final ImageTypeSpecifier RGB555 = ImageTypeSpecifiers.createFromBufferedImageType(BUFFERED_IMAGE_TYPE_RGB555);
 
     public I64ImageReader(final ImageReaderSpi provider) {
         super(provider);
@@ -79,229 +67,149 @@ public final class I64ImageReader extends ImageReaderBase {
 
     @Override
     protected void resetMembers() {
-
     }
 
     @Override
     public int getWidth(final int imageIndex) throws IOException {
-        return 150;
+        return 100;
     }
 
     @Override
     public int getHeight(final int imageIndex) throws IOException {
-        return 100;
+        return 50;
     }
 
     @Override
     public Iterator<ImageTypeSpecifier> getImageTypes(final int imageIndex) throws IOException {
         ImageTypeSpecifier rawType = getRawImageType(imageIndex);
-
         List<ImageTypeSpecifier> specifiers = new ArrayList<>();
-
-        // TODO: Implement
         specifiers.add(rawType);
-
         return specifiers.iterator();
     }
 
     @Override
     public ImageTypeSpecifier getRawImageType(final int imageIndex) throws IOException {
-
-        return GRAYSCALE;
-
-//        int channels = header.getChannels();
-//
-//        switch (header.getBitsPerPixel()) {
-//            case 1:
-//            case 2:
-//            case 4:
-//                // TODO: If there's a VGA palette here, use it?
-//
-//                return ImageTypeSpecifiers.createFromIndexColorModel(header.getEGAPalette());
-//            case 8:
-//                if (channels == 1) {
-//                    // We may have IndexColorModel here for 1 channel images
-//                    IndexColorModel palette = getVGAPalette();
-//
-//                    if (palette != null) {
-//                        return ImageTypeSpecifiers.createFromIndexColorModel(palette);
-//                    }
-//                    else {
-//                        // PCX Gray has 1 channel and no palette
-//                        return ImageTypeSpecifiers.createGrayscale(8, DataBuffer.TYPE_BYTE);
-//                    }
-//                }
-//
-//                // PCX RGB has channels for 24 bit RGB, will be validated by ImageTypeSpecifier
-//                return ImageTypeSpecifiers.createBanded(ColorSpace.getInstance(ColorSpace.CS_sRGB), createIndices(channels, 1), createIndices(channels, 0), DataBuffer.TYPE_BYTE, channels == 4, false);
-//            case 24:
-//                // Some sources says this is possible...
-//                return ImageTypeSpecifiers.createFromBufferedImageType(BufferedImage.TYPE_3BYTE_BGR);
-//            case 32:
-//                // Some sources says this is possible...
-//                return ImageTypeSpecifiers.createFromBufferedImageType(BufferedImage.TYPE_4BYTE_ABGR);
-//            default:
-//                throw new IIOException("Unknown number of bytes per pixel: " + header.getBitsPerPixel());
-//        }
+        return RGB555;
     }
-
 
 
     @Override
     public BufferedImage read(final int imageIndex, final ImageReadParam param) throws IOException {
+        assertInput();
+        if (imageIndex != 0) {
+            throw new IndexOutOfBoundsException("Only one image expected.");
+        }
 
-        return new BufferedImage(getWidth(imageIndex), getHeight(imageIndex), TYPE_INT_RGB); // TODO
+        BufferedImage bufferedImage = new BufferedImage(getWidth(imageIndex), getHeight(imageIndex), BUFFERED_IMAGE_TYPE_RGB555);
 
-//        Iterator<ImageTypeSpecifier> imageTypes = getImageTypes(imageIndex);
-//        ImageTypeSpecifier rawType = getRawImageType(imageIndex);
-//
-//        int width = getWidth(imageIndex);
-//        int height = getHeight(imageIndex);
-//
-//        BufferedImage destination = getDestination(param, imageTypes, width, height);
-//
-//        Rectangle srcRegion = new Rectangle();
-//        Rectangle destRegion = new Rectangle();
-//        computeRegions(param, width, height, destination, srcRegion, destRegion);
-//
-//        WritableRaster destRaster = clipToRect(destination.getRaster(), destRegion, param != null ? param.getDestinationBands() : null);
-//        checkReadParamBandSettings(param, rawType.getNumBands(), destRaster.getNumBands());
-//
-//        int compression = header.getCompression();
-//
-//        // Wrap input (COMPRESSION_RLE is really the only value allowed)
-//        DataInput input = compression == PCX.COMPRESSION_RLE
-//                          ? new DataInputStream(new DecoderStream(IIOUtil.createStreamAdapter(imageInput), new RLEDecoder()))
-//                          : imageInput;
-//
-//        int xSub = param != null ? param.getSourceXSubsampling() : 1;
-//        int ySub = param != null ? param.getSourceYSubsampling() : 1;
-//
-//        processImageStarted(imageIndex);
-//
-//        if (rawType.getColorModel() instanceof IndexColorModel && header.getChannels() > 1) {
-//            // Bit planes!
-//            // Create raster from a default 8 bit layout
-//            int planeWidth = header.getBytesPerLine();
-//            int rowWidth = planeWidth * 8; // bitsPerPixel == 1
-//            WritableRaster rowRaster = GRAYSCALE.createBufferedImage(rowWidth, 1).getRaster();
-//
-//            // Clip to source region
-//            Raster clippedRow = clipRowToRect(rowRaster, srcRegion,
-//                    param != null ? param.getSourceBands() : null,
-//                    param != null ? param.getSourceXSubsampling() : 1);
-//
-//            byte[] planeData = new byte[rowWidth];
-//            byte[] rowDataByte = ((DataBufferByte) rowRaster.getDataBuffer()).getData();
-//
-//            for (int y = 0; y < height; y++) {
-//                switch (header.getBitsPerPixel()) {
-//                    case 1:
-//                        readRowByte(input, srcRegion, xSub, ySub, planeData, 0, planeWidth * header.getChannels(), destRaster, clippedRow, y);
-//                        break;
-//                    default:
-//                        throw new AssertionError();
-//                }
-//
-//                int pixelPos = 0;
-//                for (int planePos = 0; planePos < planeWidth; planePos++) {
-//                    BitRotator.bitRotateCW(planeData, planePos, planeWidth, rowDataByte, pixelPos, 1);
-//                    pixelPos += 8;
-//                }
-//
-//                processImageProgress(100f * y / height);
-//
-//                if (y >= srcRegion.y + srcRegion.height) {
-//                    break;
-//                }
-//
-//                if (abortRequested()) {
-//                    processReadAborted();
-//                    break;
-//                }
-//            }
-//        }
-//        else if (header.getBitsPerPixel() == 24 || header.getBitsPerPixel() == 32) {
-//            // Can't use width here, as we need to take bytesPerLine into account, and re-create a width based on this
-//            int rowWidth = (header.getBytesPerLine() * 8) / header.getBitsPerPixel();
-//            WritableRaster rowRaster = rawType.createBufferedImage(rowWidth, 1).getRaster();
-//
-//            // Clip to source region
-//            Raster clippedRow = clipRowToRect(rowRaster, srcRegion,
-//                    param != null ? param.getSourceBands() : null,
-//                    param != null ? param.getSourceXSubsampling() : 1);
-//
-//            for (int y = 0; y < height; y++) {
-//                byte[] rowDataByte = ((DataBufferByte) rowRaster.getDataBuffer()).getData();
-//                readRowByte(input, srcRegion, xSub, ySub, rowDataByte, 0, rowDataByte.length, destRaster, clippedRow, y);
-//
-//                processImageProgress(100f * y / height);
-//
-//                if (y >= srcRegion.y + srcRegion.height) {
-//                    break;
-//                }
-//
-//                if (abortRequested()) {
-//                    processReadAborted();
-//                    break;
-//                }
-//            }
-//        }
-//        else {
-//            // Can't use width here, as we need to take bytesPerLine into account, and re-create a width based on this
-//            int rowWidth = (header.getBytesPerLine() * 8) / header.getBitsPerPixel();
-//            WritableRaster rowRaster = rawType.createBufferedImage(rowWidth, 1).getRaster();
-//
-//            // Clip to source region
-//            Raster clippedRow = clipRowToRect(rowRaster, srcRegion,
-//                    param != null ? param.getSourceBands() : null,
-//                    param != null ? param.getSourceXSubsampling() : 1);
-//
-//            for (int y = 0; y < height; y++) {
-//                for (int c = 0; c < header.getChannels(); c++) {
-//                    WritableRaster destChannel = destRaster.createWritableChild(destRaster.getMinX(), destRaster.getMinY(), destRaster.getWidth(), destRaster.getHeight(), 0, 0, new int[] {c});
-//                    Raster srcChannel = clippedRow.createChild(clippedRow.getMinX(), 0, clippedRow.getWidth(), 1, 0, 0, new int[] {c});
-//
-//                    switch (header.getBitsPerPixel()) {
-//                        case 1:
-//                        case 2:
-//                        case 4:
-//                        case 8:
-//                            byte[] rowDataByte = ((DataBufferByte) rowRaster.getDataBuffer()).getData(c);
-//                            readRowByte(input, srcRegion, xSub, ySub, rowDataByte, 0, rowDataByte.length, destChannel, srcChannel, y);
-//                            break;
-//                        default:
-//                            throw new AssertionError();
-//                    }
-//
-//                    if (abortRequested()) {
-//                        break;
-//                    }
-//                }
-//
-//                processImageProgress(100f * y / height);
-//
-//                if (y >= srcRegion.y + srcRegion.height) {
-//                    break;
-//                }
-//
-//                if (abortRequested()) {
-//                    processReadAborted();
-//                    break;
-//                }
-//            }
-//        }
-//
-//        processImageComplete();
-//
-//        return destination;
+        int[] colorPalette = new int[64]; // really only 16-bit are used
+        for (int i = 0; i < colorPalette.length; i++) {
+            colorPalette[i] = read(imageInput) + read(imageInput) * 256;
+        }
+
+        // This is a high-score table of the most frequently occurring pixel
+        // pairs. We store the 15bit RGB value of each pixel. So, if the third most
+        // popular pair is {black,red} we'd store [2][0]=0x0000 [2][1]=0x7c00.
+        int[][] dictionary = new int[64][2]; // really only 16-bit are used in the resulting int, directly a color of a pixel
+        for (int i = 0; i < dictionary.length; i++) {
+            dictionary[i][0] = read(imageInput) + read(imageInput) * 256;
+            dictionary[i][1] = read(imageInput) + read(imageInput) * 256;
+        }
+
+        int flipTableSize = read(imageInput) + read(imageInput) * 256;
+        if (DEBUG) {
+            System.err.println("Flip table size is: " + flipTableSize);
+        }
+        int[] flipTable = new int[flipTableSize-2];
+        for (int i = 0; i < flipTableSize-2; i++) { // subtract the two size bytes
+            flipTable[i] = read(imageInput);
+        }
+
+        // Now we're finally at the compressed image data.
+        // The encoding is like this:
+        //  00cccccc        Single pixel, colour of index C (0-63)
+        //  01cccccc        Two pixels, colour of index C (0-63)
+        //  10nnnnnn        Two pixels, dictionary lookup entry N (0-63)
+        //  11cccccc        Run of pixels, colour of index C (0-63)
+        //  rrrrrrrr        Run length is R+3 (thus 3 to 258)
+
+
+        int[][] pixels = new int[getHeight(imageIndex)][getWidth(imageIndex)];
+
+        for (int line = 0; line < getHeight(imageIndex); line++) {
+            if (DEBUG){
+                System.err.println("Line: "+line);
+            }
+            int pixelCount = 0; // per line
+            while (pixelCount < getWidth(imageIndex)) {
+                int value = read(imageInput);
+                int opcode = value & 192;
+                value &= 63;
+                switch (opcode) {
+                    case 0:
+                        pixels[line][pixelCount++] = colorPalette[value];
+                        break;
+                    case 64:
+                        pixels[line][pixelCount++] = colorPalette[value];
+                        pixels[line][pixelCount++] = colorPalette[value];
+                        break;
+                    case 128:
+                        // TODO: use flipTable to see if these pixels here need to be flipped around
+                        pixels[line][pixelCount++] = dictionary[value][0];
+                        pixels[line][pixelCount++] = dictionary[value][1];
+                        break;
+                    case 192:
+                        int len = read(imageInput);
+                        for (int i = 0; i < len + 3; i++) {
+                            if (DEBUG){
+                                System.err.println("Run of pixels, number: "+(len+3));
+                            }
+                            pixels[line][pixelCount++] = colorPalette[value];
+                        }
+                        break;
+                    default:
+                        throw new IOException("Internal error.");
+                }
+            }
+        }
+
+        if (DEBUG) {
+            int c;
+            while ((c = imageInput.read()) != -1) {
+                System.err.println("Trailing garbage: " + c);
+            }
+        }
+
+        copyPixels(imageIndex, pixels, bufferedImage);
+
+        processImageComplete();
+        return bufferedImage;
     }
 
+    private void copyPixels(int imageIndex, int[][] source, BufferedImage dest) throws IOException {
+        WritableRaster raster = dest.getRaster();
+        for (int line = 0; line < getHeight(imageIndex); line++) {
+            for (int col = 0; col < getWidth(imageIndex); col++) {
+                int red = dest.getColorModel().getRed(source[line][col]);
+                int green = dest.getColorModel().getGreen(source[line][col]);
+                int blue = dest.getColorModel().getBlue(source[line][col]);
+                raster.setPixel(col, line, new int[]{red, green, blue});
+            }
+        }
+    }
+
+    private int read(ImageInputStream is) throws IOException {
+        int val = is.read();
+        if (val == -1) {
+            throw new IOException("Short read.");
+        }
+        return val;
+    }
 
 
     @Override
     public IIOMetadata getImageMetadata(final int imageIndex) throws IOException {
-        return null;
+        return null; // no metadata available.
     }
 
 }
